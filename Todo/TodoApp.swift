a95b3900e3e9fceea70e202f2f6afd92a968becf
Tonnykh <tonnykhumbongmayum@gmail.com>
2023-07-02 14:10:59 +0530

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Khumbongmayum Tonny on 02/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
